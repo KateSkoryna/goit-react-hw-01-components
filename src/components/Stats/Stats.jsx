cb90ded929a41e329e5c4200a4e8d07981288f37
Kateryna Skoryna <k.skoryna@gmail.com>
2022-09-11 16:05:56 +0200

    Added propTypes to stats

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -41,8 +41,10 @@ export const Stats = ({ stats: { followers, views, likes } }) => {
   );
 };
 
-Stats.propTypes = PropTypes.shape({
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
-}).isRequired;
+Stats.propTypes = {
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
